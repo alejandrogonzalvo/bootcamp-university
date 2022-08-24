5f776ff2f4ab592823972d0e86290ba156e11d78
Alejandro <alejandrogonhid@gmail.com>
2022-08-24 11:22:04 +0200

    first commit

---
+++ UniversityApiBackend/Models/DataModels/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public enum Level
+    {
+        Basic = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    public class BaseEntity
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual User CreatedBy { get; set; } = new User();
+        public User UpdatedBy { get; set; } = new User();
+        public DateTime? UpdatedAt { get; set; }
+        public User DeletedBy { get; set; } = new User();
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course: BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public string TargetAudience { get; set; } = string.Empty;
+        
+        public string Objectives { get; set; } = string.Empty;
+        
+        public string Requirements { get; set; } = string.Empty;
+
+        public Level Level { get; set; }
+
+        [Required]
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+
+        [Required]
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+
+    }
+}
